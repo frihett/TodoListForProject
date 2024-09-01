9e11c319050b7b80dbf3444151ed600d960b4001
sseung <sseutta@naver.com>
2024-09-01 23:18:25 +0900

    feat: Schedule 객체를 받아 처리함

--- lib/controllers/schedule_controller.dart
+++ lib/controllers/schedule_controller.dart
@@ -2,15 +2,15 @@ import 'package:get/get.dart';
 import 'package:todo_list_for_project_1/models/schedule.dart';
 
 class ScheduleController extends GetxController {
-  var schedules = <Schedule>[].obs;
+  final schedules = <Schedule>[].obs;
 
-  //일정 생성하기
+  //일정 생성
   void addSchedule(Schedule schedule) {
     schedules.add(schedule);
   }
 
-  //일정 삭제하기
+  //일정 삭제
   void removeSchedule(Schedule schedule) {
-    schedules.remove(schedule);  // 일정 삭제
+    schedules.remove(schedule);
   }
 }

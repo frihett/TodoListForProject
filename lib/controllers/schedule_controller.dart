025f3015805c420a232b94804e3adaf5cd61f0e7
sseung <sseutta@naver.com>
2024-09-01 13:36:16 +0900

    feat: ScehduleController 추가

---
+++ lib/controllers/schedule_controller.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+import 'package:todo_list_for_project_1/models/schedule.dart';
+
+class ScheduleController extends GetxController{
+  var schedules = <Schedule>[].obs;
+
+
+}

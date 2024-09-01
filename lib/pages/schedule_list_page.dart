5381b4d5cca2509d2b210e31ba91dd61f9f782cc
sseung <sseutta@naver.com>
2024-09-01 14:47:48 +0900

    feat: 섹션추가, 스크롤뷰 추가
    SinglechildScrollView, ScheduleSection 추가

--- lib/pages/schedule_list_page.dart
+++ lib/pages/schedule_list_page.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list_for_project_1/widgets/schedule_section.dart';
 
 class ScheduleListPage extends StatelessWidget {
   const ScheduleListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Schedule'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ScheduleSection(),
+            ScheduleSection(),
+            ScheduleSection(),
+            ScheduleSection()
+          ],
+        ),
+      ),
+    );
   }
 }

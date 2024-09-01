d7ab05ca97baab4a471995240f9a6c2834ecc9eb
sseung <sseutta@naver.com>
2024-09-02 00:55:53 +0900

    refactor: UI 수정
    - 일정아이템 테두리 색상,두께 추가
    - 각 로우 패딩추가
    - 스케줄 디테일 색상,글꼴, 글자크기 변경

--- lib/widgets/schedule_item.dart
+++ lib/widgets/schedule_item.dart
@@ -9,32 +9,60 @@ class ScheduleItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.all(4.0),
       child: Container(
           decoration: BoxDecoration(
             color: Colors.white,
             borderRadius: BorderRadius.circular(8.0),
+            border: Border.all(
+              color: Colors.grey[300]!,
+              width: 1.5,
+            ),
           ),
           child: Column(
             children: [
-              Row(
-                children: [
-                  Text(schedule.title),
-                  Spacer(),
-                  Icon(Icons.add),
-                ],
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Row(
+                  children: [
+                    Text(schedule.title),
+                    Spacer(),
+                    Icon(Icons.add),
+                  ],
+                ),
               ),
-              Row(
-                children: [
-                  Text(schedule.detail),
-                ],
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+                child: Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.purple[100],
+                        borderRadius: BorderRadius.circular(4.0),
+                      ),
+                      padding: EdgeInsets.all(4.0),
+                      child: Text(
+                        schedule.detail,
+                        style: TextStyle(
+                          color: Colors.purple[800],
+                          fontSize: 10.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              Row(
-                children: [
-                  Text(schedule.taskId),
-                  Spacer(),
-                  Icon(Icons.circle),
-                ],
+              Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Row(
+                  children: [
+                    Text(schedule.taskId),
+                    Spacer(),
+                    Icon(Icons.circle),
+                  ],
+                ),
               ),
             ],
           )),

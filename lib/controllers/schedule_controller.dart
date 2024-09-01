7fdf615c7a66535665e1d5f408a91176b2fe5774
sseung <sseutta@naver.com>
2024-09-01 13:37:06 +0900

    feat: 일정 삭제하기 메소드 추가

--- lib/controllers/schedule_controller.dart
+++ lib/controllers/schedule_controller.dart
@@ -1,13 +1,16 @@
 import 'package:get/get.dart';
 import 'package:todo_list_for_project_1/models/schedule.dart';
 
-class ScheduleController extends GetxController{
+class ScheduleController extends GetxController {
   var schedules = <Schedule>[].obs;
 
-//일정 생성하기
-  void addSchedule(Schedule schedule){
+  //일정 생성하기
+  void addSchedule(Schedule schedule) {
     schedules.add(schedule);
   }
 
-
+  //일정 삭제하기
+  void removeSchedule(Schedule schedule) {
+    schedules.remove(schedule);  // 일정 삭제
+  }
 }

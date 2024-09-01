2250ab1a80bcf663f75f0729ea6dff81ed1880e2
sseung <sseutta@naver.com>
2024-09-01 23:28:25 +0900

    feat: ScheduleController 형 컨트롤러를 사용하여 일정 상태별로 필터후 일정 리스트를 ScheduleSection으로 전달

--- lib/pages/schedule_list_page.dart
+++ lib/pages/schedule_list_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todo_list_for_project_1/controllers/schedule_controller.dart';
+import 'package:todo_list_for_project_1/models/schedule_status.dart';
 import 'package:todo_list_for_project_1/widgets/schedule_section.dart';
 
 class ScheduleListPage extends StatelessWidget {
@@ -6,40 +9,63 @@ class ScheduleListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ScheduleController controller = Get.find<ScheduleController>();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Schedule'),
       ),
       body: SingleChildScrollView(
-        child: Row(
-          children: [
-
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ScheduleSection(),
+        child: Obx(() {
+          return Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ScheduleSection(
+                    title: '할 일',
+                    schedule: controller.schedules
+                        .where((s) => s.status == ScheduleStatus.todo)
+                        .toList(),
+                  ),
+                ),
               ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ScheduleSection(),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ScheduleSection(
+                    title: '급한 일',
+                    schedule: controller.schedules
+                        .where((s) => s.status == ScheduleStatus.urgent)
+                        .toList(),
+                  ),
+                ),
               ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ScheduleSection(),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ScheduleSection(
+                    title: '진행 중',
+                    schedule: controller.schedules
+                        .where((s) => s.status == ScheduleStatus.inProgress)
+                        .toList(),
+                  ),
+                ),
               ),
-            ),
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ScheduleSection(),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ScheduleSection(
+                    title: '완료',
+                    schedule: controller.schedules
+                        .where((s) => s.status == ScheduleStatus.completed)
+                        .toList(),
+                  ),
+                ),
               ),
-            )
-          ],
-        ),
+            ],
+          );
+        }),
       ),
     );
   }

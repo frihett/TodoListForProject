e2e4d5ba0a56d671d39eccd6ad7d2f0b5a34749c
sseung <sseutta@naver.com>
2024-09-01 23:25:10 +0900

    refactor: Schedule 객체를 받아 텍스트 처리

--- lib/widgets/schedule_item.dart
+++ lib/widgets/schedule_item.dart
@@ -1,31 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list_for_project_1/models/schedule.dart';
 
 class ScheduleItem extends StatelessWidget {
-  const ScheduleItem({super.key});
+  final Schedule schedule;
+
+  const ScheduleItem({super.key, required this.schedule});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
-          decoration: BoxDecoration(color: Colors.red,borderRadius: BorderRadius.circular(8.0),),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(8.0),
+          ),
           child: Column(
             children: [
               Row(
                 children: [
-                  Text('상담 견적서 발행1'),
+                  Text(schedule.title),
                   Spacer(),
                   Icon(Icons.add),
                 ],
               ),
               Row(
                 children: [
-                  Text('상담'),
+                  Text(schedule.detail),
                 ],
               ),
               Row(
                 children: [
-                  Text('CRM-199'),
+                  Text(schedule.taskId),
                   Spacer(),
                   Icon(Icons.circle),
                 ],

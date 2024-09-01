ab6e6d590d49a73fceccc55779ccc42d4ae4d8b5
sseung <sseutta@naver.com>
2024-09-01 23:25:50 +0900

    refactor: List<Schedule> 를 받고 ScheduleItem으로 전달

--- lib/widgets/schedule_section.dart
+++ lib/widgets/schedule_section.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:todo_list_for_project_1/models/schedule.dart';
 import 'package:todo_list_for_project_1/widgets/schedule_item.dart';
 
 class ScheduleSection extends StatelessWidget {
-  const ScheduleSection({super.key});
+  final String title;
+  final List<Schedule> schedule;
+
+  const ScheduleSection(
+      {super.key, required this.title, required this.schedule});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(color: Colors.brown),
+      decoration: BoxDecoration(color: Colors.grey),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Text('할일'),
+            child: Text(title),
           ),
-          ScheduleItem(),
-          ScheduleItem(),
+          // 여러 개의 ScheduleItem을 생성 스프레드 연산자 사용
+          ...schedule
+              .map((schedule) => ScheduleItem(schedule: schedule))
+              .toList(),
         ],
       ),
     );

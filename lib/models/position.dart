681408ebe9c2d5a868f2f3f39ccf1c0597c6ad7e
sseung <sseutta@naver.com>
2024-09-01 13:11:08 +0900

    feat: Position enum 추가

---
+++ lib/models/position.dart
@@ -0,0 +1,13 @@
+enum Position {
+  Director, // 이사
+  VicePresident, // 부사장
+  Manager, // 부장
+  TeamLead, // 팀장
+
+  Developer, // 개발자
+  Designer, // 디자이너
+  HRManager, // 인사 관리자
+  HR, // 인사 담당자
+  MarketingManager, // 마케팅 관리자
+  Marketing, // 마케팅 담당자
+}

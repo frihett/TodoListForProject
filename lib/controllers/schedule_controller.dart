2677f86d8af204a31f91a2e72ff2124c5be01a23
sseung <sseutta@naver.com>
2024-09-01 13:36:39 +0900

    feat: 일정 생성하기 메소드 추가

--- lib/controllers/schedule_controller.dart
+++ lib/controllers/schedule_controller.dart
@@ -4,5 +4,10 @@ import 'package:todo_list_for_project_1/models/schedule.dart';
 class ScheduleController extends GetxController{
   var schedules = <Schedule>[].obs;
 
+//일정 생성하기
+  void addSchedule(Schedule schedule){
+    schedules.add(schedule);
+  }
+
 
 }

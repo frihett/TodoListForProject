099078bc4be0492b6c451623313e1779933666ad
sseung <sseutta@naver.com>
2024-09-01 14:44:06 +0900

    feat: ScheduleSection 위젯(컴포넌트) 추가

---
+++ lib/widgets/schedule_section.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ScheduleSection extends StatelessWidget {
+  const ScheduleSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

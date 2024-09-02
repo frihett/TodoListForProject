163a063be703fda1f1467172abed70a5a805dd8e
sseung <sseutta@naver.com>
2024-09-03 00:51:57 +0900

    feat: Get.dialog(Dialog) 추가
    GestureDectector로 아이템 클릭시 ScheduleEditPage 다이얼로그로 전환

--- lib/widgets/schedule_item.dart
+++ lib/widgets/schedule_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:todo_list_for_project_1/models/schedule.dart';
+import 'package:todo_list_for_project_1/pages/schedule_edit_page.dart';
 
 class ScheduleItem extends StatelessWidget {
   final Schedule schedule;
@@ -8,64 +10,73 @@ class ScheduleItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(4.0),
-      child: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(8.0),
-            border: Border.all(
-              color: Colors.grey[300]!,
-              width: 1.5,
-            ),
-          ),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: Row(
-                  children: [
-                    Text(schedule.title),
-                    Spacer(),
-                    Icon(Icons.add),
-                  ],
-                ),
+    return GestureDetector(
+      onTap: () {
+        Get.dialog(Dialog(
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
+          child: ScheduleEditPage(schedule: schedule,),
+        ));
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8.0),
+              border: Border.all(
+                color: Colors.grey[300]!,
+                width: 1.5,
               ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
-                child: Row(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        color: Colors.purple[100],
-                        borderRadius: BorderRadius.circular(4.0),
-                      ),
-                      padding: EdgeInsets.all(4.0),
-                      child: Text(
-                        schedule.detail,
-                        style: TextStyle(
-                          color: Colors.purple[800],
-                          fontSize: 10.0,
-                          fontWeight: FontWeight.bold,
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Row(
+                    children: [
+                      Text(schedule.title),
+                      Spacer(),
+                      Icon(Icons.add),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 4.0, horizontal: 8.0),
+                  child: Row(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.purple[100],
+                          borderRadius: BorderRadius.circular(4.0),
+                        ),
+                        padding: EdgeInsets.all(4.0),
+                        child: Text(
+                          schedule.subTitle,
+                          style: TextStyle(
+                            color: Colors.purple[800],
+                            fontSize: 10.0,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: Row(
-                  children: [
-                    Text(schedule.taskId),
-                    Spacer(),
-                    Icon(Icons.circle),
-                  ],
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Row(
+                    children: [
+                      Text(schedule.taskId),
+                      Spacer(),
+                      Icon(Icons.circle),
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          )),
+              ],
+            )),
+      ),
     );
   }
 }

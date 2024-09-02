7e6da4fb58a013ada1eaa61f6de5567fdd06ab94
sseung <sseutta@naver.com>
2024-09-02 13:51:35 +0900

    feat: 일정 생성하기 버튼 추가
    ScheduleCreatePage로 이동

--- lib/pages/schedule_list_page.dart
+++ lib/pages/schedule_list_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:todo_list_for_project_1/controllers/schedule_controller.dart';
 import 'package:todo_list_for_project_1/models/schedule_status.dart';
+import 'package:todo_list_for_project_1/pages/schedule_create_page.dart';
 import 'package:todo_list_for_project_1/widgets/schedule_section.dart';
 
 class ScheduleListPage extends StatelessWidget {
@@ -14,65 +15,86 @@ class ScheduleListPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text('Schedule'),
-        backgroundColor:  Colors.white,
+        title: const Text('Schedule'),
+        backgroundColor: Colors.white,
       ),
-      body: SingleChildScrollView(
-        child: Obx(() {
-          return Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ScheduleSection(
-                    title: '할 일',
-                    schedule: controller.schedules
-                        .where((s) => s.status == ScheduleStatus.todo)
-                        .toList(),
-                    color: Colors.blue.withOpacity(0.05),
-                  ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Get.to(() => ScheduleCreatePage());
+                  },
+                  child: const Text('일정 생성하기'),
                 ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ScheduleSection(
-                    title: '급한 일',
-                    schedule: controller.schedules
-                        .where((s) => s.status == ScheduleStatus.urgent)
-                        .toList(),
-                    color: Colors.red.withOpacity(0.05),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ScheduleSection(
-                    title: '진행 중',
-                    schedule: controller.schedules
-                        .where((s) => s.status == ScheduleStatus.inProgress)
-                        .toList(),
-                    color: Colors.orange.withOpacity(0.05),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ScheduleSection(
-                    title: '완료',
-                    schedule: controller.schedules
-                        .where((s) => s.status == ScheduleStatus.completed)
-                        .toList(),
-                    color: Colors.green.withOpacity(0.05),
-                  ),
-                ),
-              ),
-            ],
-          );
-        }),
+              ],
+            ),
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: Obx(() {
+                return Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ScheduleSection(
+                          title: '할 일',
+                          schedule: controller.schedules
+                              .where((s) => s.status == ScheduleStatus.todo)
+                              .toList(),
+                          color: Colors.blue.withOpacity(0.05),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ScheduleSection(
+                          title: '급한 일',
+                          schedule: controller.schedules
+                              .where((s) => s.status == ScheduleStatus.urgent)
+                              .toList(),
+                          color: Colors.red.withOpacity(0.05),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ScheduleSection(
+                          title: '진행 중',
+                          schedule: controller.schedules
+                              .where(
+                                  (s) => s.status == ScheduleStatus.inProgress)
+                              .toList(),
+                          color: Colors.orange.withOpacity(0.05),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ScheduleSection(
+                          title: '완료',
+                          schedule: controller.schedules
+                              .where(
+                                  (s) => s.status == ScheduleStatus.completed)
+                              .toList(),
+                          color: Colors.green.withOpacity(0.05),
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              }),
+            ),
+          ),
+        ],
       ),
     );
   }

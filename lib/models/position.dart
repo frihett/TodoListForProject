a08616f2a969a87acf78b9db986971fe84c5c7bc
sseung <sseutta@naver.com>
2024-09-02 22:39:13 +0900

    refactor: 변수 네이밍 로우카멜케이스로 적용

--- lib/models/position.dart
+++ lib/models/position.dart
@@ -1,13 +1,13 @@
 enum Position {
-  Director, // 이사
-  VicePresident, // 부사장
-  Manager, // 부장
-  TeamLead, // 팀장
+  director, // 이사
+  vicePresident, // 부사장
+  manager, // 부장
+  teamLead, // 팀장
 
-  Developer, // 개발자
-  Designer, // 디자이너
-  HRManager, // 인사 관리자
-  HR, // 인사 담당자
-  MarketingManager, // 마케팅 관리자
-  Marketing, // 마케팅 담당자
+  developer, // 개발자
+  designer, // 디자이너
+  hrManager, // 인사 관리자
+  hr, // 인사 담당자
+  marketingManager, // 마케팅 관리자
+  marketing, // 마케팅 담당자
 }

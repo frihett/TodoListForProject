45f2b3e005e071df1b233821f1c8b58f2ebe59e1
sseung <sseutta@naver.com>
2024-09-04 02:20:08 +0900

    feat: 드래그 기능 추가
    - LongPressDraggable 위젯으로 감싸기
    - global키로 높이, 너비를 구해서 feedback 속성에 적용

--- lib/widgets/schedule_item.dart
+++ lib/widgets/schedule_item.dart
@@ -3,22 +3,179 @@ import 'package:get/get.dart';
 import 'package:todo_list_for_project_1/models/schedule.dart';
 import 'package:todo_list_for_project_1/pages/schedule_edit_page.dart';
 
-class ScheduleItem extends StatelessWidget {
+class ScheduleItem extends StatefulWidget {
   final Schedule schedule;
 
   const ScheduleItem({super.key, required this.schedule});
 
   @override
+  State<ScheduleItem> createState() => _ScheduleItemState();
+}
+
+class _ScheduleItemState extends State<ScheduleItem> {
+  final GlobalKey _key = GlobalKey();
+  Size _widgetSize = Size.zero;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+// 위젯이 빌드된 후 크기를 측정합니다.
+      final RenderBox? renderBox =
+          _key.currentContext?.findRenderObject() as RenderBox?;
+      if (renderBox != null) {
+        setState(() {
+          _widgetSize = renderBox.size;
+        });
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Get.dialog(Dialog(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
-          child: ScheduleEditPage(schedule: schedule,),
-        ));
-      },
-      child: Padding(
+    return LongPressDraggable<Schedule>(
+      data: widget.schedule,
+      feedback: Material(
+        color: Colors.transparent,
+        child: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Container(
+              width: _widgetSize.width,
+              height: _widgetSize.height,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8.0),
+                border: Border.all(
+                  color: Colors.grey[300]!,
+                  width: 1.5,
+                ),
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Row(
+                      children: [
+                        Text(widget.schedule.title),
+                        Spacer(),
+                        Icon(Icons.add),
+                      ],
+                    ),
+                  ),
+                  if (widget.schedule.subTitle != '')
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 4.0, horizontal: 8.0),
+                      child: Row(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.purple[100],
+                              borderRadius: BorderRadius.circular(4.0),
+                            ),
+                            padding: EdgeInsets.all(4.0),
+                            child: Text(
+                              widget.schedule.subTitle!,
+                              style: TextStyle(
+                                color: Colors.purple[800],
+                                fontSize: 10.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Row(
+                      children: [
+                        Text(widget.schedule.taskId),
+                        Spacer(),
+                        Icon(Icons.circle),
+                      ],
+                    ),
+                  ),
+                ],
+              )),
+        ),
+      ),
+      child: GestureDetector(
+        onTap: () {
+          Get.dialog(Dialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12.0)),
+            child: Container(
+                width: 800,
+                height: 800,
+                child: ScheduleEditPage(
+                  schedule: widget.schedule,
+                )),
+          ));
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(4.0),
+          child: Container(
+              key: _key, // GlobalKey 를 적용합니다.
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(8.0),
+                border: Border.all(
+                  color: Colors.grey[300]!,
+                  width: 1.5,
+                ),
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Row(
+                      children: [
+                        Text(widget.schedule.title),
+                        Spacer(),
+                        Icon(Icons.add),
+                      ],
+                    ),
+                  ),
+                  if (widget.schedule.subTitle != '')
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 4.0, horizontal: 8.0),
+                      child: Row(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.purple[100],
+                              borderRadius: BorderRadius.circular(4.0),
+                            ),
+                            padding: EdgeInsets.all(4.0),
+                            child: Text(
+                              widget.schedule.subTitle!,
+                              style: TextStyle(
+                                color: Colors.purple[800],
+                                fontSize: 10.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Row(
+                      children: [
+                        Text(widget.schedule.taskId),
+                        Spacer(),
+                        Icon(Icons.circle),
+                      ],
+                    ),
+                  ),
+                ],
+              )),
+        ),
+      ),
+      childWhenDragging: Padding(
         padding: const EdgeInsets.all(4.0),
         child: Container(
             decoration: BoxDecoration(
@@ -35,40 +192,41 @@ class ScheduleItem extends StatelessWidget {
                   padding: const EdgeInsets.all(4.0),
                   child: Row(
                     children: [
-                      Text(schedule.title),
+                      Text(widget.schedule.title),
                       Spacer(),
                       Icon(Icons.add),
                     ],
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 4.0, horizontal: 8.0),
-                  child: Row(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Colors.purple[100],
-                          borderRadius: BorderRadius.circular(4.0),
-                        ),
-                        padding: EdgeInsets.all(4.0),
-                        child: Text(
-                          schedule.subTitle,
-                          style: TextStyle(
-                            color: Colors.purple[800],
-                            fontSize: 10.0,
-                            fontWeight: FontWeight.bold,
+                if (widget.schedule.subTitle != '')
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        vertical: 4.0, horizontal: 8.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.purple[100],
+                            borderRadius: BorderRadius.circular(4.0),
+                          ),
+                          padding: EdgeInsets.all(4.0),
+                          child: Text(
+                            widget.schedule.subTitle!,
+                            style: TextStyle(
+                              color: Colors.purple[800],
+                              fontSize: 10.0,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
                 Padding(
                   padding: const EdgeInsets.all(4.0),
                   child: Row(
                     children: [
-                      Text(schedule.taskId),
+                      Text(widget.schedule.taskId),
                       Spacer(),
                       Icon(Icons.circle),
                     ],

c8c3e35e423cc6cb1798948b2d53262f78d51b59
sseung <sseutta@naver.com>
2024-09-03 13:34:19 +0900

    feat: 필드 널러블로 변경
    생성자 수정, == 연산자 수정

--- lib/models/schedule.dart
+++ lib/models/schedule.dart
@@ -3,34 +3,36 @@ import 'package:todo_list_for_project_1/models/schedule_status.dart';
 
 class Schedule {
   final String title;
-  final Employee responsiblePerson; // 담당자
-  final String subTitle;
-  final String description;
-  final DateTime dueDate;
+  final Employee? responsiblePerson; // 담당자
+  final String? subTitle;
+  final String? description;
+  final DateTime? dueDate;
   final ScheduleStatus status;
   final String taskId;
 
-  Schedule(
-      {required this.title,
-      required this.responsiblePerson,
-      required this.subTitle,
-      required this.dueDate,
-      required this.status,
-      required this.taskId,
-      required this.description });
+  Schedule({
+    this.responsiblePerson,
+    this.subTitle,
+    this.description,
+    this.dueDate,
+    required this.title,
+    required this.status,
+    required this.taskId,
+  });
 
   @override
-  bool operator ==(Object other) =>
-      identical(this, other) ||
-      other is Schedule &&
-          runtimeType == other.runtimeType &&
-          title == other.title &&
-          responsiblePerson == other.responsiblePerson &&
-          subTitle == other.subTitle &&
-          dueDate == other.dueDate &&
-          status == other.status &&
-          taskId == other.taskId &&
-          description == other.description;
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is Schedule &&
+        other.title == title &&
+        other.responsiblePerson == responsiblePerson &&
+        other.subTitle == subTitle &&
+        other.description == description &&
+        other.dueDate == dueDate &&
+        other.status == status &&
+        other.taskId == taskId;
+  }
 
   @override
   int get hashCode =>
@@ -50,8 +52,8 @@ class Schedule {
   Schedule copyWith({
     String? title,
     Employee? responsiblePerson,
-    String? detail,
-    DateTime? date,
+    String? subTitle,
+    DateTime? dueDate,
     ScheduleStatus? status,
     String? taskId,
     String? description,
@@ -59,11 +61,11 @@ class Schedule {
     return Schedule(
       title: title ?? this.title,
       responsiblePerson: responsiblePerson ?? this.responsiblePerson,
-      subTitle: detail ?? this.subTitle,
-      dueDate: date ?? this.dueDate,
+      subTitle: subTitle ?? this.subTitle,
+      dueDate: dueDate ?? this.dueDate,
       status: status ?? this.status,
       taskId: taskId ?? this.taskId,
-        description: description ?? this.description,
+      description: description ?? this.description,
     );
   }
 }

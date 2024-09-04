5696866dcac0ecb7fb1ffdd6a2d76610641ea4c9
sseung <sseutta@naver.com>
2024-09-04 12:45:10 +0900

    feat: 드랍 기능 구현
    섹션 부분을 DragTarget위젯으로 감싸기
    섹션 제목에 맞게 스케줄의 상태를 업데이트 하고 스케줄을 업데이트

--- lib/widgets/schedule_section.dart
+++ lib/widgets/schedule_section.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:todo_list_for_project_1/controllers/schedule_controller.dart';
 import 'package:todo_list_for_project_1/models/schedule.dart';
+import 'package:todo_list_for_project_1/models/schedule_status.dart';
 import 'package:todo_list_for_project_1/widgets/schedule_item.dart';
 
 class ScheduleSection extends StatelessWidget {
@@ -7,29 +10,54 @@ class ScheduleSection extends StatelessWidget {
   final List<Schedule> schedule;
   final Color color;
 
-  const ScheduleSection(
-      {super.key, required this.title, required this.schedule, required this.color});
+  ScheduleSection({
+    super.key,
+    required this.title,
+    required this.schedule,
+    required this.color,
+  });
+
+  final ScheduleController controller = Get.find<ScheduleController>();
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(8.0),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(title),
+    return DragTarget<Schedule>(
+      onAcceptWithDetails: (details) {
+        final receivedSchedule = details.data;
+
+        // 섹션 제목에 맞게 스케줄 상태를 업데이트합니다.
+        final updatedSchedule = receivedSchedule.copyWith(
+          status: ScheduleStatus.fromTitle(title: title),
+        );
+
+        // 기존의 스케줄을 업데이트 된 스케줄로 교체합니다.
+        controller.updateSchedule(
+          oldSchedule: receivedSchedule,
+          newSchedule: updatedSchedule,
+        );
+      },
+      builder: (context, candidateData, rejectedData) {
+        return Container(
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(8.0),
           ),
-          // 여러 개의 ScheduleItem을 생성 스프레드 연산자 사용
-          ...schedule
-              .map((schedule) => ScheduleItem(schedule: schedule))
-              .toList(),
-        ],
-      ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('$title  ${schedule.length}'),
+              ),
+              Column(
+                children: schedule
+                    .map((schedule) => ScheduleItem(schedule: schedule))
+                    .toList(),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }

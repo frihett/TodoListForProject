26e3573207219fbd0e9188790897951e98eca1f5
sseung <sseutta@naver.com>
2024-09-04 17:26:56 +0900

    feat: 드래그앤 드랍 기능 추가
    _ getTargetIndex 추가
    - 일정 사이에 일정 넣기 로직 추가

--- lib/widgets/schedule_section.dart
+++ lib/widgets/schedule_section.dart
@@ -19,11 +19,23 @@ class ScheduleSection extends StatelessWidget {
 
   final ScheduleController controller = Get.find<ScheduleController>();
 
+  // 드래그가 떨어진 위치에 해당하는 인덱스를 반환하는 함수
+  int _getTargetIndex(double offsetY) {
+
+    final itemHeight = 100.0; // 각 아이템의 예상 높이
+    if (schedule.isEmpty) {
+      return 0; // 기본 인덱스 반환
+    }
+    final index = (offsetY / itemHeight).floor();
+    return index.clamp(0, schedule.length - 1);
+  }
+
   @override
   Widget build(BuildContext context) {
     return DragTarget<Schedule>(
       onAcceptWithDetails: (details) {
         final receivedSchedule = details.data;
+
 
         // 섹션 제목에 맞게 스케줄 상태를 업데이트합니다.
         final updatedSchedule = receivedSchedule.copyWith(
@@ -35,6 +47,26 @@ class ScheduleSection extends StatelessWidget {
           oldSchedule: receivedSchedule,
           newSchedule: updatedSchedule,
         );
+
+        final oldIndex = schedule.indexOf(receivedSchedule);
+        final newIndex = _getTargetIndex(details.offset.dy);
+
+        // 유효한 인덱스인지 확인
+        if (oldIndex == -1) {
+          // 올바른 인덱스가 아닐 경우 처리
+          return;
+        }
+
+        if (oldIndex != newIndex) {
+          // 새로운 순서로 업데이트
+          final updatedSchedules = List<Schedule>.from(schedule);
+          updatedSchedules.removeAt(oldIndex);
+          updatedSchedules.insert(newIndex, receivedSchedule);
+
+
+          // 컨트롤러에 업데이트된 리스트를 전달
+          controller.updateSchedulesInSection(title: title, updatedSchedules: updatedSchedules);
+        }
       },
       builder: (context, candidateData, rejectedData) {
         return Container(
